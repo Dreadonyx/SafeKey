c9f8a6ebf4fc9794a9cb1db4197e0532d50afede
Dreadonyx <>
2026-03-30 18:05:51 +0530

    minimal theme,mobile layout, Android APK fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,13 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(not(target_os = "android"))]
+    {
+        builder = builder.plugin(tauri_plugin_opener::init());
+    }
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running SafeKey");
 }
